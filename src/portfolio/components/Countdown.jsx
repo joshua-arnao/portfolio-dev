2de88e3da6edcc10dcfc6b1e965d5513d88b511c
joshua-arnao <joshua.arnao@icloud.com>
2024-04-07 00:16:59 -0500

    fix error

--- src/portfolio/components/Countdown.jsx
+++ src/portfolio/components/Countdown.jsx
@@ -14,7 +14,8 @@ const firebaseConfig = {
   storageBucket: import.meta.env.VITE_REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env
     .VITE_REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_REACT_APP_FIREBASE_APP_ID
+  appId: import.meta.env.VITE_REACT_APP_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 export const Countdown = () => {
