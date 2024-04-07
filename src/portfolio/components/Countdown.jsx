a63b6adaff2a61d8af83b217ce64d602fe3ee9cb
joshua-arnao <joshua.arnao@icloud.com>
2024-04-07 17:45:19 -0500

    new try correctión fixed

--- src/portfolio/components/Countdown.jsx
+++ src/portfolio/components/Countdown.jsx
@@ -1,20 +1,19 @@
 import { useState, useEffect } from 'react';
 import Typography from '@mui/material/Typography';
-// import dotenv from 'dotenv';
-// dotenv.config();
 
 import { initializeApp } from 'firebase/app';
 import { getDatabase, ref, get, set } from 'firebase/database';
 // import { getAnalytics } from 'firebase/analytics';
 
-const apiKey = import.meta.env.VITE_FIREBASE_API_KEY;
-const authDomain = import.meta.env.VITE_FIREBASE_AUTH_DOMAIN;
-const databaseURL = import.meta.env.VITE_BASE_URL;
-const projectId = import.meta.env.VITE_FIREBASE_PROJECT_ID;
-const storageBucket = import.meta.env.VITE_FIREBASE_STORAGE_BUCKET;
-const messagingSenderId = import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID;
-const appId = import.meta.env.VITE_FIREBASE_APP_ID;
-const measurementId = import.meta.env.VITE_FIREBASE_MEASUREMENT_ID;
+const apiKey = import.meta.env.VITE_REACT_APP_FIREBASE_API_KEY;
+const authDomain = import.meta.env.VITE_REACT_APP_FIREBASE_AUTH_DOMAIN;
+const databaseURL = import.meta.env.VITE_REACT_APP_FIREBASE_BASE_URL;
+const projectId = import.meta.env.VITE_REACT_APP_FIREBASE_PROJECT_ID;
+const storageBucket = import.meta.env.VITE_REACT_APP_FIREBASE_STORAGE_BUCKET;
+const messagingSenderId = import.meta.env
+  .VITE_REACT_APP_FIREBASE_MESSAGING_SENDER_ID;
+const appId = import.meta.env.VITE_REACT_APP_FIREBASE_APP_ID;
+const measurementId = import.meta.env.VITE_REACT_APP_FIREBASE_MEASUREMENT_ID;
 
 const firebaseConfig = {
   apiKey: apiKey,
@@ -28,6 +27,7 @@ const firebaseConfig = {
 };
 
 export const Countdown = () => {
+  console.log();
   const [countdown, setCountdown] = useState(null);
 
   useEffect(() => {
