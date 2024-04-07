a8e92d6748e910e113b2cdb45d65a7807533dcae
joshua-arnao <joshua.arnao@icloud.com>
2024-04-07 16:26:12 -0500

    update of date firebase

--- src/portfolio/components/Countdown.jsx
+++ src/portfolio/components/Countdown.jsx
@@ -10,7 +10,7 @@ import { getDatabase, ref, get, set } from 'firebase/database';
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  databaseURL: import.meta.env.VITE_FIREBASE_BASE_URL,
+  databaseURL: import.meta.env.VITE_BASE_URL,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
